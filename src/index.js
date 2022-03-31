a09f0f1627bdc6113108d5fac247ab904852f256
davincecode <hyperscript.ca@gmail.com>
2022-03-30 23:21:30 -0600

    Update commit for push

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const PORT = process.env.PORT || 8001;
-const ENV = require("./environment");
+const PORT = process.env.PORT || 8001
+const ENV = require("./environment")
 
-const app = require("./application")(ENV, { updateAppointment });
-const server = require("http").Server(app);
+const app = require("./application")(ENV, { updateAppointment })
+const server = require("http").Server(app)
 
-const WebSocket = require("ws");
-const wss = new WebSocket.Server({ server });
+const WebSocket = require("ws")
+const wss = new WebSocket.Server({ server })
 
-wss.on("connection", socket => {
-  socket.onmessage = event => {
-    console.log(`Message Received: ${event.data}`);
+wss.on("connection", (socket) => {
+  socket.onmessage = (event) => {
+    console.log(`Message Received: ${event.data}`)
 
     if (event.data === "ping") {
-      socket.send(JSON.stringify("pong"));
+      socket.send(JSON.stringify("pong"))
     }
-  };
-});
+  }
+})
 
 function updateAppointment(id, interview) {
   wss.clients.forEach(function eachClient(client) {
@@ -24,13 +24,13 @@ function updateAppointment(id, interview) {
         JSON.stringify({
           type: "SET_INTERVIEW",
           id,
-          interview
+          interview,
         })
-      );
+      )
     }
-  });
+  })
 }
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT} in ${ENV} mode.`);
-});
+  console.log(`Listening on port ${PORT} in ${ENV} mode.`)
+})
